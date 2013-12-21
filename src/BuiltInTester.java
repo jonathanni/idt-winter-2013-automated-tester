bfc3357aeb04ca506a46f6543a6f906534680b8f
Kent Ma <tnekent@gmail.com>
2013-12-21 12:19:41 -0500

    First commit

---
+++ src/BuiltInTester.java
@@ -0,0 +1,20 @@
+public class BuiltInTester
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+
+    /*
+     * Example usage:
+     *   if (inputValue > 2) {
+     *       BuiltInTester.log("return true");
+     *       return true;
+     *   }
+     *   
+     */
+    public static String log(String expected_string)
+    {
+    
+    }
+}
