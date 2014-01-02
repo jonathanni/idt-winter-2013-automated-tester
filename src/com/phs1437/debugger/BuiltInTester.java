5de03944eb70334a83dfda9ca905a995f5c1a15e
jonathan_ni <jonathan.r.ni@gmail.com>
2014-01-01 22:28:12 -0500

    Add Extra Functions to Do Mutable Testing of Variables
    Add functions to allow variables to change before testing with the
    expected values. The expected values are also allowed to change. These
    functions use Object wrappers in order to accomplish the feat.

--- src/com/phs1437/debugger/BuiltInTester.java
+++ src/com/phs1437/debugger/BuiltInTester.java
@@ -1,5 +1,9 @@
 package com.phs1437.debugger;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * TODO: Implement tracking of symbols in code
  * TODO: Implement custom streams (such as outputting to file)
@@ -8,110 +12,144 @@ package com.phs1437.debugger;
 
 public class BuiltInTester implements Debugger
 {
-    /**
-    Indicates whether or not the builtin tester is enabled.
-    */
 
-    private boolean isEnabled;
+	/**
+	 * Indicates whether or not the builtin tester is enabled.
+	 */
 
-    /**
-    Default constructor.
-    Options
-      enabled: false
-    */
+	private boolean isEnabled;
 
-    public BuiltInTester()
-    {
-      this(false);
-    }
+	private HashMap<String, ArrayList<Object>> expectedValues = new HashMap<String, ArrayList<Object>>();
+	private HashMap<String, Object> givenValues = new HashMap<String, Object>();
 
-    /**
-    Constructor with one argument.
-    Options
-      enabled: choice
-    */
+	/**
+	 * Default constructor. Options enabled: false
+	 */
 
-    public BuiltInTester(boolean enable)
-    {
-      if(enable)
-        enable();
-      else
-        disable();
-    }
+	public BuiltInTester()
+	{
+		this(false);
+	}
 
-    public void expecting(int inputValue, int possibleValue, String expectedString)
-    {
-    }
+	/**
+	 * Constructor with one argument. Options enabled: choice
+	 */
 
-    public void expecting(byte inputValue, byte possibleValue, String expectedString)
-    {
-    }
+	public BuiltInTester(boolean enable)
+	{
+		if (enable)
+			enable();
+		else
+			disable();
+	}
 
-    public void expecting(short inputValue, short possibleValue, String expectedString)
-    {
-    }
+	public void expecting(int inputValue, int possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(long inputValue, long possibleValue, String expectedString)
-    {
-    }
+	public void expecting(byte inputValue, byte possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(float inputValue, float possibleValue, String expectedString)
-    {
-    }
+	public void expecting(short inputValue, short possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(double inputValue, double possibleValue, String expectedString)
-    {
-    }
+	public void expecting(long inputValue, long possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(boolean inputValue, boolean possibleValue, String expectedString)
-    {
-    }
+	public void expecting(float inputValue, float possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(char inputValue, char possibleValue, String expectedString)
-    {
-    }
+	public void expecting(double inputValue, double possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(String inputValue, String possibleValue, String expectedString)
-    {
-    }
+	public void expecting(boolean inputValue, boolean possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    public void expecting(int[] inputValue, int[] possibleValue, String expectedString)
-    {
-    }
+	public void expecting(char inputValue, char possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    /*
-     * Example usage:
-     *   if (inputValue > 2) {
-     *       BuiltInTester.log("return true");
-     *       return true;
-     *   }
-     *   
-     */
+	public void expecting(String inputValue, String possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    /**
-    Log what is expected in a variable and a String to debug.
-    */
-    public void log(String expectedString)
-    {
-      System.out.println(expectedString);
-      // TODO: Implement piping to any OutputStream
-    }
+	public void expecting(int[] inputValue, int[] possibleValue,
+			String expectedString, String variableID)
+	{
+	}
 
-    /**
-    Enable the builtin tester.
-    Any functions called with the builtin tester will register after it is enabled.
-    */
-    public void enable()
-    {
-      isEnabled = true;
-    }
+	/**
+	 * Test function: allows the variable to change, being tested only when the
+	 * log function is called.
+	 */
 
-    /**
-    Disable the builtin tester.
-    Any functions called with the builtin tester will be ignored after it is disabled.
-    */
-    public void disable()
-    {
-      isEnabled = false;
-    }
+	public void expectingMutable(Object mutableInputValue,
+			Object mutablePossibleValue, String expectedString,
+			String variableID)
+	{
+	}
+
+	public void expectingMutable(Object[] mutableInputValue,
+			Object[] mutablePossibleValue, String expectedString,
+			String variableID)
+	{
+	}
+
+	/*
+	 * Example usage: if (inputValue > 2) { BuiltInTester.log("return true");
+	 * return true; }
+	 */
+
+	/**
+	 * Log what is expected (given in the expecting* functions) and if the block
+	 * of code PASSED or FAILED.
+	 */
+	public void log(String expectedString)
+	{
+		System.out.println(expectedString);
+		// TODO: Implement piping to any OutputStream
+	}
+
+	/**
+	 * Enable the builtin tester. Any functions called with the builtin tester
+	 * will register after it is enabled.
+	 */
+	public void enable()
+	{
+		setEnabled(true);
+	}
+
+	/**
+	 * Disable the builtin tester. Any functions called with the builtin tester
+	 * will be ignored after it is disabled.
+	 */
+	public void disable()
+	{
+		setEnabled(false);
+	}
+
+	public boolean isEnabled()
+	{
+		return isEnabled;
+	}
+
+	private void setEnabled(boolean isEnabled)
+	{
+		this.isEnabled = isEnabled;
+	}
 }
--- src/com/phs1437/debugger/Debugger.java
+++ src/com/phs1437/debugger/Debugger.java
@@ -8,50 +8,67 @@
  */
 package com.phs1437.debugger;
 
-public interface Debugger 
+public interface Debugger
 {
-    /*
-     * Creates the basic test.
-     * Has 3 arguments:
-     *    1 - A variable
-     *    2 - Expected value of the variable
-     *    3 - Expected string to be logged
-     *
-     * Example Usage:
-     *     BuiltinTester.expecting(inputValue, 0, "return false")
-     *
-     * TODO: There is probably a better way of doing this. Find what that is.
-     */
-    public void expecting(int inputValue, int possibleValue, String expectedString);
-    public void expecting(byte inputValue, byte possibleValue, String expectedString);
-    public void expecting(short inputValue, short possibleValue, String expectedString);
-    public void expecting(long inputValue, long possibleValue, String expectedString);
-    public void expecting(float inputValue, float possibleValue, String expectedString);
-    public void expecting(double inputValue, double possibleValue, String expectedString);
-    public void expecting(boolean inputValue, boolean possibleValue, String expectedString);
-    public void expecting(char inputValue, char possibleValue, String expectedString);
-    public void expecting(String inputValue, String possibleValue, String expectedString);
-    public void expecting(int[] inputValue, int[] possibleValue, String expectedString);
-     
-    /*
-     * Logs :field:`expectedString`
-     * Example usage:
-     *   if (inputValue > 2) {
-     *       BuiltInTester.log("return true");
-     *       return true;
-     *   }
-     */
-    public void log(String expectedString);
-    
-    /*
-     * Enables and disables the BuiltInTester
-     * Has 0 arguments
-     *
-     * Example Usage:
-     *     BuiltInTester.enable();
-     */
-     
-    public void enable();
-  
-    public void disable();
+	/*
+	 * Creates the basic test. Has 3 arguments: 1 - A variable 2 - Expected
+	 * value of the variable 3 - Expected string to be logged
+	 * 
+	 * Example Usage: BuiltinTester.expecting(inputValue, 0, "return false")
+	 * 
+	 * TODO: There is probably a better way of doing this. Find what that is.
+	 */
+	public void expecting(int inputValue, int possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(byte inputValue, byte possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(short inputValue, short possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(long inputValue, long possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(float inputValue, float possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(double inputValue, double possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(boolean inputValue, boolean possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(char inputValue, char possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(String inputValue, String possibleValue,
+			String expectedString, String variableID);
+
+	public void expecting(int[] inputValue, int[] possibleValue,
+			String expectedString, String variableID);
+
+	public void expectingMutable(Object mutableInputValue,
+			Object mutablePossibleValue, String expectedString,
+			String variableID);
+	
+	public void expectingMutable(Object[] mutableInputValue,
+			Object[] mutablePossibleValue, String expectedString,
+			String variableID);
+
+	/*
+	 * Logs :field:`expectedString` Example usage: if (inputValue > 2) {
+	 * BuiltInTester.log("return true"); return true; }
+	 */
+	public void log(String expectedString);
+
+	/*
+	 * Enables and disables the BuiltInTester Has 0 arguments
+	 * 
+	 * Example Usage: BuiltInTester.enable();
+	 */
+
+	public void enable();
+
+	public void disable();
 }
