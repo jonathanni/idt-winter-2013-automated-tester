951d598fb29a6d0860c478e816427c534c3c3303
Kent Ma <tnekent@gmail.com>
2014-01-10 15:55:59 +0000

    cleaned up comments

--- src/com/phs1437/debugger/Debugger.java
+++ src/com/phs1437/debugger/Debugger.java
@@ -16,42 +16,9 @@ public interface Debugger
 	 * Creates the basic test. Has 3 arguments: 1 - A variable 2 - Expected
 	 * value of the variable 3 - Expected string to be logged
 	 * 
-	 * Example Usage: BuiltinTester.expecting(inputValue, 0, "return false")
-	 * 
-	 * TODO: There is probably a better way of doing this. Find what that is.
+	 * Example Usage: BuiltinTester.expecting(inputValue, 0, "return false", )
 	 */
 
-	/*
-	 * public void expecting(int inputValue, int possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(byte inputValue, byte possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(short inputValue, short possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(long inputValue, long possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(float inputValue, float possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(double inputValue, double possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(boolean inputValue, boolean possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(char inputValue, char possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(String inputValue, String possibleValue, String
-	 * expectedString, String variableID);
-	 * 
-	 * public void expecting(int[] inputValue, int[] possibleValue, String
-	 * expectedString, String variableID);
-	 */
 	public void expecting(Object mutableInputValue,
 			Object mutablePossibleValue, String expectedString,
 			String variableID, String functionID, Class<?> type);
