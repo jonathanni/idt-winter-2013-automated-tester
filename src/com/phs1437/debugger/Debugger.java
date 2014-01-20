64be300a565fcf512fa9d9a34c6cf152420c001a
Kent Ma <tnekent@gmail.com>
2014-01-20 04:58:23 -0500

    Added some javadocs

--- src/com/phs1437/debugger/Debugger.java
+++ src/com/phs1437/debugger/Debugger.java
@@ -1,22 +1,38 @@
 package com.phs1437.debugger;
 
-/*
+/**
  * Debugger Interface
  * ~~~~~~~~~~~~~~~~~~~
  * Outlines the general project structure: the required methods, fields, etc.
  *
- * Copyright: (c) 2013 by Diwakar Ganesan, Kent Ma, Jonathan Ni
- * License: spaghetti, see LICENSE for details
+ * @author phs_winter2013, Diwakar Ganesan, Kent Ma, Jonathan Ni
  */
-
-
 public interface Debugger
 {
-	/*
-	 * Creates the basic test. Has 3 arguments: 1 - A variable 2 - Expected
-	 * value of the variable 3 - Expected string to be logged
-	 * 
-	 * Example Usage: BuiltinTester.expecting(inputValue, 0, "return false", )
+	/**
+	 * Creates the basic test
+     * * @param mutableInputValue    The variable to be tested.
+     *
+     * @param mutablePossibleValue A possible value for the variable.
+     *
+     * @param expectedString       Specifies the string needed to be logged in 
+     *                             order to pass the test. Use {@link 
+     *                             #log(String)} to actually log the string 
+     *                             specified for this. 
+     *
+     * @param variableID           A unique string that identifies the variable 
+     *                             inputted. This is used to differentiate 
+     *                             between different tests simultaneously 
+     *                             running for {@link #log(String)}
+     *
+     * @param functionID           A unique string to identify the function the 
+     *                             tested code resided in.
+     *
+     * @param type                 The data type of the variable to be tested.
+     * 
+     * @param numInputs
+     *
+     * @param numOutputs
 	 */
 
 	public void expecting(Object mutableInputValue,
@@ -25,21 +41,30 @@ public interface Debugger
 
 	public void expecting(Object[] mutableInputValue,
 			Object[] mutablePossibleValue, String expectedString,
-			String variableID, String functionID, Class<?> type, int numInputs, int numOutputs);
+			String variableID, String functionID, Class<?> type, int numInputs, 
+            int numOutputs);
 
-	/*
-	 * Logs :field:`expectedString` Example usage: if (inputValue > 2) {
-	 * BuiltInTester.log("return true"); return true; }
+	/**
+	 * Logs `expectedString` 
+     *
+     * Example usage:
+     *     if (inputValue > 2) {
+     *         BuiltInTester.log("return true");
+     *         return true;
+     *     }
+     *
+     * @param expectedString String to log. See {@link #expected(Object, Object,
+     *                       String, String, String, Class<?>)} for the use of
+     *                       this string.
+     *
 	 */
 	public void log(String expectedString);
 
-	/*
-	 * Enables and disables the BuiltInTester Has 0 arguments
-	 * 
-	 * Example Usage: BuiltInTester.enable();
+	/**
+	 * Enables and disables the BuiltInTester 
+	 * Example Usage:
+     *     BuiltInTester.enable();
 	 */
-
 	public void enable();
-
 	public void disable();
 }
