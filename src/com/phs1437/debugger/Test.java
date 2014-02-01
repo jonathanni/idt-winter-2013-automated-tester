f01b621237fd2627206c8afc0e18001a057f76b1
jonathan_ni <jonathan.r.ni@gmail.com>
2014-02-01 16:42:26 -0500

    Add More Testing Functions to Test.java

--- src/com/phs1437/debugger/Test.java
+++ src/com/phs1437/debugger/Test.java
@@ -6,6 +6,48 @@ public class Test
 	{
 		isEven(3);
 		isEven(2);
+
+		count_to_5();
+		print2();
+	}
+
+	/* Trivial example: function that prints 2 */
+
+	public static void print2()
+	{
+		BuiltInTester tester = new BuiltInTester(true); // Initializes + enables
+														// the tester
+		int numberToPrint = 3;
+		/* Setting up the basic test */
+		tester.expecting(numberToPrint, 3, 2, "numberToPrint",
+				"public void print2(String)", Integer.class, Integer.class);
+		/*
+		 * BUG BELOW: numberToPrint isn’t 2, so the function has the wrong
+		 * output.
+		 */
+		System.out.println(numberToPrint);
+		tester.log("numberToPrint", numberToPrint);
+	}
+
+	public static void count_to_5()
+	{
+		BuiltInTester tester = new BuiltInTester(true); // Initializes + enables
+														// the tester
+		int numberList[] = { 1, 2, 3, 4, 4 };
+		/* Setting up the basic test */
+		tester.expecting(numberList, new int[] { 1, 2, 3, 4, 4 }, new int[] {
+				1, 2, 3, 4, 5 }, "numberList", "count_to_5()", Integer.class,
+				Integer.class);
+		/*
+		 * BUG BELOW: numberList doesn’t count from 1 to 5, so the function
+		 * prints “1,2,3,4,4” instead of “1,2,3,4,5”
+		 */
+		for (int i : numberList)
+		{
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		tester.log("numberList", numberList);
 	}
 
 	public static boolean isEven(int x)
